f21fc03e5bc1f48a0a69d7ba64636f7b041a565e
Jiameng Wu <jiameng.wu@gmail.com>
2023-10-31 15:44:55 +0100

    change input arguments

--- 02_code/rename.py
+++ 02_code/rename.py
@@ -10,13 +10,14 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description="Rename all files in a given folder that match a certain expression")
-parser.add_argument('inputs', nargs=3, help='-i directory -i match -i replace_by')
+parser.add_argument('inputs', help='{inputs}')
+parser.add_argument('match', help='expression')
+parser.add_argument('replace_by', help='expression')
 args = parser.parse_args()
-directory = args.inputs[0]
-match = args.inputs[1]
-replace_by = args.inputs[2]
+directory = args.inputs
+match = args.match
+replace_by = args.replace_by
 
 old_file_names = os.listdir(directory)
 new_file_names = [i.replace(match, replace_by) for i in old_file_names]
-[os.replace(os.path.join(directory, old), os.path.join(directory, new))
- for (old, new) in zip(old_file_names, new_file_names)]
+[os.replace(os.path.join(directory, old), os.path.join(directory, new)) for (old, new) in zip(old_file_names, new_file_names)]
